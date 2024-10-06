f5aa4e060aafed46cba74e25f6b7113393382c77
Alexrore <alejandro.rodriguez.orellana@estudiantat.upc.edu>
2024-10-06 23:11:35 +0200

    Add files via upload

--- Servidor.c
+++ Servidor.c
@@ -1,24 +1,53 @@
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <mysql.h>
+#include <pthread.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <stdbool.h>
 #include <netinet/in.h>
-#include <stdio.h>
-#include <mysql/mysql.h>
+#include <pthread.h>
+#include <time.h>
+
+
+MYSQL *conn;
+
+void conectarBD() {
+	
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexion: %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	conn = mysql_real_connect (conn, "shiva2.upc.es","alejandro.rodriguez.orellana", "0111043V", "BD_V1", 0, NULL, 0);
+	//conn = mysql_real_connect (conn, "localhost","root", "mysql", NULL, 0, NULL, 0);
+	if (conn==NULL)
+	{
+		printf ("Error al inicializar la conexion: %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+}
 
 int main(int argc, char *argv[])
 {
 	int sock_conn, sock_listen, ret;
 	struct sockaddr_in serv_adr;
-	char buff[512];
-	char buff2[512];
+	char peticion[512];
+	char respuesta[512];
 	// INICIALITZACIONS
 	// Obrim el socket
 	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		printf("Error creant socket");
-	// Fem el bind al port
 	
+	// Fem el bind al port
 	
 	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
 	serv_adr.sin_family = AF_INET;
@@ -42,6 +71,31 @@ int main(int argc, char *argv[])
 		sock_conn = accept(sock_listen, NULL, NULL);
 		printf ("He recibido conexion\n");
 		//sock_conn es el socket que usaremos para este cliente
+		ret=read(sock_conn, peticion, sizeof(peticion));
+		peticion[ret]='\0';
+		
+		char *p = strtok(peticion, "/");
+		int codigo = atoi (p);
+		p = strtok( NULL, "/");
+		char nombre[20];
+		strcpy(nombre, p);
+		printf("Codigo: %d, Nombre: %s\n", codigo, nombre);
+		
+		if (codigo==1) //register
+		{
+			
+		}
+			
+		else if (codigo==2)// Log in
+		{
+			
+		}
+		
+		else if(codigo==3)// Consulta
+		{
+			
+		}
+			
 		
 		
 		// Se acabo el servicio para este cliente
